d241f754e65716761bb79e91bc33125cbdcf7e5d
ghi <gustavoghioldi@gmail.com>
2017-01-28 15:57:10 -0300

    para mostarrle a masi

--- themes/assets/js/benefit.js
+++ themes/assets/js/benefit.js
@@ -1,3 +1,29 @@
-$(document).ready(function() {
-    
+$(function () {
+  parseFooter();
+  
 });
+
+var parseFooter = function(){
+
+  $.get( "../themes/assets/templates/footer.hbs", function( data ) {
+     var theTemplateScript = data;
+     // Compile the template
+     var theTemplate = Handlebars.compile(theTemplateScript);
+
+     // Define our data object (pude ser por get)
+      var context={
+       "city": "London",
+       "street": "Baker Street",
+        "number": "221B"
+      };
+      $.get("../themes/assets/templates/test.json", function(data){
+
+      // Pass our data to the template
+     var theCompiledHtml = theTemplate(context);
+
+      // Add the compiled html to the page
+      $('.content-footer').html(theCompiledHtml);
+      });
+
+    });
+}
